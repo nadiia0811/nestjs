e6cd213aa4f9d02deaebe94a36b9e63258aeeeb5
Nadiia Krzesinska <nb1985@ukr.net>
2025-08-26 11:48:48 +0000

    fix(entity): add default name to User entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -5,7 +5,7 @@ export class User {
   @PrimaryGeneratedColumn({ type: "int" })
   id: number;
 
-  @Column({ type: "varchar", nullable: false, length: 100 })
+  @Column({ type: "varchar", nullable: false, length: 100, default: "Anonymous" })
   name: string;
 
   @Column({ type: "varchar", length: 100, unique: true, nullable: false })
