995555317e4fc2172fd49aa3ea0a58c2d325b069
Nadiia Krzesinska <nb1985@ukr.net>
2025-08-26 08:23:25 +0000

    chore(app.module): switch Postgres connection to IPv4

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,32 +6,28 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { User } from "./entity/User";
 
-
 @Module({
-   controllers: [AppController],
-   providers: [AppService],
-   imports: [
-        ConfigModule.forRoot({
-          isGlobal: true
-        }),
-     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get("DB_HOST"),
-        port: Number(configService.get("DB_PORT")),
-        username: configService.get("DB_USER"),
-        password: configService.get("DB_PASSWORD"),
-        database: configService.get("DB_NAME"),
-        entities: [User], 
-        synchronize: true,
-      }),
-      inject: [ConfigService],
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
-     TypeOrmModule.forFeature([User]),
-     UsersModule  
-   ]
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: "postgres",
+          url: configService.get<string>("POSTGRES_DATABASE_URL"),
+          entities: [User],
+          synchronize: true,
+          ssl: { rejectUnauthorized: false },
+        };
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+    UsersModule,
+  ],
 })
-export class AppModule {
-
-}
+export class AppModule {}
