045103219a4689ec6ed9a6c6b4181a2628a56abb
Nadiia Krzesinska <nb1985@ukr.net>
2025-08-26 09:30:47 +0000

    feat(user): create User entity

---
+++ src/entity/User.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({ type: "int" })
+  id: number;
+
+  @Column({ type: "varchar", nullable: false, length: 100 })
+  name: string;
+
+  @Column({ type: "varchar", length: 100, unique: true, nullable: false })
+  email: string;
+
+  @Column({ type: "varchar", nullable: false })
+  password: string;
+
+  @Column({ type: "boolean", default: false })
+  banned: boolean;
+
+  @Column({ type: "varchar", nullable: true })
+  banReason: string;
+
+  @CreateDateColumn({ type: "timestamp", name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamp", name: "updated_at" })
+  updatedAt: Date;
+}
