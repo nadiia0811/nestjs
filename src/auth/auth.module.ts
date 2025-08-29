c72159b112180abfc4875aba7872d42ece952215
Nadiia Krzesinska <nb1985@ukr.net>
2025-08-29 07:32:58 +0000

    refactor(users, auth): use forwardRef to resolve circular dependencies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 
+
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
   imports: [
-    UsersModule, 
+    forwardRef(() => UsersModule),
     JwtModule.register({
       secret: process.env.PRIVATE_KEY || "SECRET",
       signOptions: {
         expiresIn: "24h"
       }
   })],
-  exports: [AuthService]
+  exports: [AuthService, JwtModule]
 })
 export class AuthModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entity/User';
 import { RolesModule } from 'src/roles/roles.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 
 @Module({
@@ -11,7 +12,8 @@ import { RolesModule } from 'src/roles/roles.module';
   providers: [UsersService],
   imports: [
     TypeOrmModule.forFeature([User]),
-    RolesModule
+    RolesModule,
+    forwardRef(() => AuthModule)
   ],
   exports: [UsersService]
 })
